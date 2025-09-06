d359abd32677ce20822d161245d604ff428d7605
Harshit Kumar <harshitkumarhk1290@gmail.com>
2025-09-06 09:55:17 +0530

    Added logic to disable form on submit

--- src/components/Chat/PromptInput.jsx
+++ src/components/Chat/PromptInput.jsx
@@ -1,17 +1,25 @@
 import axios from "axios";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
-
 export default function PromptInput() {
   const { register, handleSubmit, reset } = useForm();
-
+  const [isLoading, setIsLoading] = useState(false);
   const onSubmit = async (data) => {
-    const response = await axios.post("http://localhost:3000", data);
-    if (response.status !== 200) {
-      console.error("Error submitting prompt:", response.statusText);
-      return;
+    setIsLoading(true);
+    try {
+      const response = await axios.post("http://localhost:3000/putdata", data);
+      if (response.status !== 200) {
+        console.error("Error submitting prompt:", response.statusText);
+        return;
+      }
+      console.log("Prompt submitted:", data.prompt);
+      console.dir(response.data);
+      reset();
+    } catch (error) {
+      console.error("An error occurred during the request:", error);
+    } finally {
+      setIsLoading(false);
     }
-    console.log("Prompt submitted:", data.prompt);
-    reset(); 
   };
 
   return (
@@ -24,11 +32,18 @@ export default function PromptInput() {
         type="text"
         placeholder="Type your prompt here..."
         {...register("prompt", { required: true })}
-        className="flex-1 bg-transparent text-gray-200 placeholder-gray-400 focus:outline-none px-4"
+        className={`flex-1 bg-transparent text-gray-200 placeholder-gray-400 focus:outline-none px-4 ${
+          isLoading ? "cursor-not-allowed text-gray-500" : null
+        }`}
+        disabled={isLoading}
       />
 
       {/* Send Button */}
-      <button type="submit" className="text-gray-400 hover:text-gray-200 pr-3">
+      <button
+        type="submit"
+        className="text-gray-400 hover:text-gray-200 pr-3"
+        disabled={isLoading}
+      >
         <svg
           xmlns="http://www.w3.org/2000/svg"
           className="w-5 h-5"
