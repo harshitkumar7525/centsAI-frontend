f166aea722a10fe9976cb3482758f5cc21a73db2
Harshit Kumar <harshitkumarhk1290@gmail.com>
2025-09-09 19:05:27 +0530

    Added vercel.json

--- apiCall.js
+++ apiCall.js
@@ -14,6 +14,7 @@ const api = axios.create({
     (error) => Promise.reject(error)
   );
   api.interceptors.response.use(
+  
     (response) => response,
     (error) => {
       if (error.response && error.response.status === 401) {
